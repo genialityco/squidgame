85653798d1d768ed7ef603e93938dfb9a21c7b83
balderban <maria.perdomo@mocion.com.co>
2025-05-15 11:39:16 -0500

    ajustando el tiempo del juego 1 que me quedo en 2 minutos y medio y era n15 segundos

--- js/gameSettings.js
+++ js/gameSettings.js
@@ -53,7 +53,7 @@ export var gameSettings = {
         players:50,
         length:85,
         dead:[5,15],
-        timer:150000
+        timer:15000
     },
     game2:{
         name:'GALLETA DALGONA',
